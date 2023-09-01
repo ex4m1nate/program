33dab534c9756137bb708a8324d58da92867233c
ex4m1nate <136436495+ex4m1nate@users.noreply.github.com>
2023-09-01 17:15:04 +0900

    No commit message

--- shellscript/OracleTablespaceCheck.sh
+++ shellscript/OracleTablespaceCheck.sh
@@ -1,24 +1,40 @@
 #!/bin/sh
 
 SQL="set feedback off;
-    set echo off;
-    set flush off;
-    set head on;
-    alter session set container = pdb;
-    col size_mb format 990.00
-    col usage_mb format 990.00
-    col free_mb format 990.00
-    col rate format 990.00
-    select
-    a.TABLESPACE_NAME
-    , min(a.BYTES)/1024/1024 as size_mb
-    , round(min(a.BYTES)/(1024*1024) - sum(b.BYTES)/ (1024*1024),2) as usage_mb
-    , round(sum(b.BYTES)/(1024*1024),2) as free_mb
-    , round((min(a.BYTES)/(1024*1024) - sum(b.BYTES)/(1024*1024))/ (min(a.BYTES)/1024/1024)*100,2) as rate
-    from dba_data_files a, dba_free_space b
-    where a.FILE_ID = b.FILE_ID
-    group by a.TABLESPACE_NAME
-    ;"
+set echo off;
+set flush off;
+set head on;
+alter session set container = pdb;
+col tablespace_name     format a15
+col "size_MB"           format 990.99
+col "used_MB"           format 990.99
+col "free_MB"           format 990.99
+col "rate"              format 990.99
+select
+    tablespace_name,
+    nvl(total_bytes / 1024 / 1024, 0) as size_MB,
+    nvl((total_bytes - free_total_bytes) / 1024 / 1024, 0) as used_MB,
+    nvl(free_total_bytes / 1024 / 1024, 0) as free_MB,
+    round(nvl((total_bytes - free_total_bytes) / total_bytes * 100,100),2) as rate
+from
+  ( select
+      tablespace_name,
+      sum(bytes) total_bytes
+    from
+      dba_data_files
+    group by
+      tablespace_name
+  ),
+  ( select
+      tablespace_name free_tablespace_name,
+      sum(bytes) free_total_bytes
+    from
+      dba_free_space
+    group by tablespace_name
+  )
+where
+  tablespace_name = free_tablespace_name(+)
+/"
 
 RESULT=`sqlplus -s / as sysdba << EOF
     $SQL
