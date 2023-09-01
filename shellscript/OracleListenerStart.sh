021806e185a1f49cfcdc1ac3edb9014c326b78c8
ex4m1nate <136436495+ex4m1nate@users.noreply.github.com>
2023-09-01 17:22:15 +0900

    No commit message

--- shellscript/OracleListenerStart.sh
+++ shellscript/OracleListenerStart.sh
@@ -5,9 +5,7 @@ lsnrctl start
 LSNRCTL_STATUS=$?
 
 if [ $LSNRCTL_STATUS != 0 ]; then
-    echo "Error: Oracle Listener could not be started."
     exit 2;
 fi
 
-echo "Oracle Listener started successfully."
 exit 0;
--- shellscript/OracleListenerStop.sh
+++ shellscript/OracleListenerStop.sh
@@ -5,9 +5,7 @@ lsnrctl stop
 LSNRCTL_STATUS=$?
 
 if [ $LSNRCTL_STATUS != 0 ]; then
-    echo "Error: Oracle Listener could not be stopped."
     exit 2;
 fi
 
-echo "Oracle Listener stopped successfully."
 exit 0;
