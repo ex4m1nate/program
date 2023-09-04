96276da18d6472b0c6b421871191807da1bad800
ex4m1nate <136436495+ex4m1nate@users.noreply.github.com>
2023-09-04 13:41:15 +0900

    No commit message

--- shellscript/OracleIndexRebuild.sh
+++ shellscript/OracleIndexRebuild.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-while IFS=' ' read -r index_name
+while IFS=' ' read -r fum_index_name
 do
-    sqlplus -s / as sysdba <<EOF
+    sqlplus -s fum/password@pdb <<EOF
     set feedback off;
     set echo off;
     set flush off;
     set head off;
-    alter session set container = pdb;
-    alter index $index_name rebuild;
+    alter index $fum_index_name rebuild;
     exit;
 EOF
 
-done < Index.lst
+done < index_fum.lst
 
 DB_STATUS=$?
 
@@ -20,6 +19,70 @@ if [ $DB_STATUS != 0  ]; then
     echo "Indexes could not be altered."
     exit 2;
 fi
+
+
+while IFS=' ' read -r fmm_index_name
+do
+    sqlplus -s fmm/password@pdb <<EOF
+    set feedback off;
+    set echo off;
+    set flush off;
+    set head off;
+    alter index $fmm_index_name rebuild;
+    exit;
+EOF
+
+done < index_fmm.lst
+
+DB_STATUS=$?
+
+if [ $DB_STATUS != 0  ]; then
+    echo "Indexes could not be altered."
+    exit 2;
+fi
+
+
+while IFS=' ' read -r fml_index_name
+do
+    sqlplus -s fml/password@pdb <<EOF
+    set feedback off;
+    set echo off;
+    set flush off;
+    set head off;
+    alter index $fml_index_name rebuild;
+    exit;
+EOF
+
+done < index_fml.lst
+
+DB_STATUS=$?
+
+if [ $DB_STATUS != 0  ]; then
+    echo "Indexes could not be altered."
+    exit 2;
+fi
+
+
+while IFS=' ' read -r ofac1_index_name
+do
+    sqlplus -s ofac1/ofac@pdb <<EOF
+    set feedback off;
+    set echo off;
+    set flush off;
+    set head off;
+    alter index $ofac1_index_name rebuild;
+    exit;
+EOF
+
+done < index_ofac1.lst
+
+DB_STATUS=$?
+
+if [ $DB_STATUS != 0  ]; then
+    echo "Indexes could not be altered."
+    exit 2;
+fi
+
 
 echo "Index altered."
 
