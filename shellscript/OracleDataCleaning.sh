be558c24bc5f82b6498611f194f2f09079b9a6e9
ex4m1nate <136436495+ex4m1nate@users.noreply.github.com>
2023-09-05 13:35:03 +0900

    No commit message

--- shellscript/OracleDataCleaning.sh
+++ shellscript/OracleDataCleaning.sh
@@ -8,42 +8,12 @@ alter session set container = pdb;
 whenever sqlerror exit 2;
 declare
     reference_date date :=sysdate - 1000;
-    cursor table_cursor is
-        select table_name
-        from all_tables
-        where table_name in (
-            'OFAC1.FIRCO_APP_LOG_PARAMETERS'
-            , 'OFAC1.FIRCO_APP_LOG'
-            , 'OFAC1.FIRCO_CTY_REPORT_UNIT'
-            , 'OFAC1.FIRCO_CTY_REPORT'
-        );
-    cursor table2_cursor is
-        select table_name
-        from all_tables
-        where table_name in (
-            'FUM.FUM_LOG'
-            , 'FML.FML_ARCHIVED_RULES'
-        );
 begin
-    for table_rec in table_cursor loop
-        begin
-            execute immediate 'delete from ' || table_rec.table_name || ' where ofac1.firco_app_log.event_time < :1' 
-                using reference_date;
-            commit;
-        end;
-    end loop;
-        begin
-            execute immediate 'delete from ' || table_rec.table_name || ' where ofac1.firco_app_log.event_time < :1' 
-                using reference_date;
-            commit;
-        end;
-    end loop;
+    delete from fum.fum_log where update_date < reference_date;
+    delete from fml.fml_archived_rules where fml_arcdate < reference_date;
+    commit;
 end;
 /
-delete from fum.fum_log where update_date < :1 
-    using reference_date;
-delete from fml.fml_archived_rules where fml_arcdate < :1
-    using reference_date;
 "
 
 sqlplus -s / as sysdba << EOF
