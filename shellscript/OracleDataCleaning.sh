b732168a37133a5b4cd0f45bb3c26025ea1537ec
ex4m1nate <136436495+ex4m1nate@users.noreply.github.com>
2023-09-05 18:08:55 +0900

    No commit message

--- shellscript/OracleDataCleaning.sh
+++ shellscript/OracleDataCleaning.sh
@@ -5,12 +5,14 @@ set echo off;
 set flush off;
 set head off;
 alter session set container = pdb;
+alter session set nls_date_format='YYYY-MM-DD HH24:MI:SS';
 whenever sqlerror exit 2;
 declare
-    reference_date date :=sysdate - 1000;
+    reference_date date;
 begin
-    delete from fum.fum_log where update_date < reference_date;
-    delete from fml.fml_archived_rules where fml_arcdate < reference_date;
+    reference_date := to_date(to_char(sysdate - 1000, 'YYYY-MM-DD HH24:MI:SS'), 'YYYY-MM-DD HH24:MI:SS');
+    delete from fum.fum_log where to_date(update_date, 'YYYY-MM-DD HH24:MI:SS') < reference_date;
+    delete from fml.fml_archived_rules where to_date(fml_arcdate, 'YYYY-MM-DD HH24:MI:SS') < reference_date;
     commit;
 end;
 /
@@ -20,10 +22,8 @@ sqlplus -s / as sysdba << EOF
     $SQL
 EOF
 
-DB_STATUS=$?
 
-
-if [ $DB_STATUS != 0  ]; then
+if [ $? != 0  ]; then
     echo "Data cleaning could not be completed."
     exit 2;
 fi
