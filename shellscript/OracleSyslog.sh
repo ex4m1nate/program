4c0514e6f40e9e5ecd85dfa4828808891b23d020
Soma Ozawa <souma.o.1026@gmail.com>
2023-09-07 14:43:53 +0900

    No commit message

--- shellscript/OracleSyslog.sh
+++ shellscript/OracleSyslog.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
 
+ALERT_LOG_PATH=/var/log/system.out
+ERROR_KEYWORDS="ORA-00600 ORA-00700"
+
+monitor_alert_log() {
+    tail -Fn0 $ALERT_LOG_PATH | while read -r line; do
+        for keyword in $ERROR_KEYWORDS; do
+            if [[ "$line" == *"$keyword"* ]]; then
+                logger -p local6.error "Oracle Alert Log: $line"
+                break
+            fi
+        done
+    done
+}
+
+monitor_alert_log
+
+
