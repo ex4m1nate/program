926a623beefeea3e2d4e502ee08b59775a336e3e
ex4m1nate <136436495+ex4m1nate@users.noreply.github.com>
2023-09-22 13:33:41 +0900

    No commit message

--- shellscript/OracleDataCleaning_2.sh
+++ shellscript/OracleDataCleaning_2.sh
@@ -14,53 +14,36 @@ whenever sqlerror exit sql.sqlcode;
 spool $logfile;
 declare
     reference_date date;
-    cursor ffu_log is
-    select
-        fum.log.username as username
-        , fum.log.update_date as update_date
-        , fum.ap.action_name as action_name
-        , fum.ap.primary_name as action
-        , fum.log.detail as detail
-        , fum.log.db as db
-        , fum.log.severity as severity
-        , fum.log.application as application
-    from
-        fum.fum_log log
-        inner join fum.fum_actiontype_primarytype ap on fum.log.primary_type = ap.primary_type
-    where
-        substr(log.update_date, 1, 10) < reference_date;
-    cursor fcvw_log is
-    select
-        user_tbl.t_login as username
-        , to_char(main_tbl.event_time, 'yyyy-mm-dd hh24:mi:ss') as event_time
-        , main_tbl.activity
-        , main_tbl.description
-    from
-        ofac1.firco_app_log main_tbl
-        , ofac1.fmf_users user_tbl
-    where
-        main_tbl.actor = user_tbl.t_id
-        and to_char(main_tbl.event_time, 'yyyymmdd') < reference_date;
 begin
-    reference_date := to_char(sysdate - 1000, 'yyyy-mm-dd hh24:mi:ss');
+    reference_date := to_date(to_char(sysdate - $limit, 'yyyy-mm-dd hh24:mi:ss'), 'yyyy-mm-dd hh24:mi:ss');
     delete
         from
-            fum.fum_log log
+            fum.fum_log
         where
-            to_char(log.update_date, 'yyyy-mm-dd hh24:mi:ss') < reference_date;
+            to_date(fum.fum_log.update_date, 'yyyy-mm-dd hh24:mi:ss') < reference_date;
     delete 
         from
             fum.fum_actiontype_primarytype ap
         where
-            ap.primary_type in (select log.primary_type from fum.fum_log log)
-            and (select log.update_date from fum.fum_log log) < reference_date;
+            ap.primary_type in (
+                select log.primary_type 
+                from fum.fum_log log 
+                where to_date(log.update_date, 'yyyy-mm-dd hh24:mi:ss') < reference_date
+            );
     delete
         from
-            ofac1.firco_app_log main_tbl
-            , ofac1.fmf_users user_tbl
+            ofac1.firco_app_log
         where
-            main_tbl.actor = user_tbl.t_id
-            and to_char(main_tbl.event_time, 'yyyy-mm-dd') < reference_date;
+            to_date(ofac1.firco_app_log.event_time, 'yyyy-mm-dd hh24:mi:ss') < reference_date;
+    delete
+        from
+            ofac1.fmf_users user_tbl
+        where
+            user_tbl.t_id in (
+                select main_tbl.actor
+                from ofac1.firco_app_log main_tbl
+                where to_date(main_tbl.event_time, 'yyyy-mm-dd hh24:mi:ss') < reference_date
+            );
     commit;
 end;
 /
